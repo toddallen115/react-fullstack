7a388b1ee176d560c2c815b478abdcd92e367ea4
Todd <allento@sas.upenn.edu>
2017-06-30 13:16:26 -0400

    webpack setup, ready for react

---
+++ reactApp/app.js
@@ -0,0 +1,7 @@
+// var React = require('react');
+import React from 'react';
+// var ReactDOM = require('react-dom');
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<p>Running</p>,
+   document.getElementById('root'));
