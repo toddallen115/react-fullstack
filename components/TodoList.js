6266511924b45c5cf6ae77b86cfffa217979a77c
Todd <allento@sas.upenn.edu>
2017-06-30 13:34:37 -0400

    static ToDoList is working

---
+++ components/TodoList.js
@@ -0,0 +1,13 @@
+const dummyData = ["Get groceries", "Finish this code", "Practice promises", "Watch fireworks"];
+
+class TodoList extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+
+    )
+  }
+}
--- reactApp/app.js
+++ reactApp/app.js
@@ -3,5 +3,26 @@ import React from 'react';
 // var ReactDOM = require('react-dom');
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>Running</p>,
+
+const dummyData = ["Get groceries", "Finish this code", "Practice promises", "Watch fireworks"];
+
+class TodoList extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <div>
+        <ul>
+          {dummyData.map( (todo) => <li><button>X</button> {todo}</li> )}
+        </ul>
+      </div>
+    )
+  }
+}
+
+
+
+ReactDOM.render(<TodoList />,
    document.getElementById('root'));
